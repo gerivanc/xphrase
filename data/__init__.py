1512a17ae6e67d7592ad196545349ae6522619a9
Gerivan Costa <119078732+gerivanc@users.noreply.github.com>
2025-11-07 21:14:10 -0400

    Initialize data module for XPhrase Generation

---
+++ data/__init__.py
@@ -0,0 +1,10 @@
+"""
+Data module for XPhrase Generation
+Contains word lists for Portuguese, English, and German languages
+"""
+
+from .german_words import GERMAN_WORDS
+from .portuguese_words import PORTUGUESE_WORDS
+from .english_words import ENGLISH_WORDS
+
+__all__ = ['GERMAN_WORDS', 'PORTUGUESE_WORDS', 'ENGLISH_WORDS']
