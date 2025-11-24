5eb9eceb484d7a2752a318a33e0b2e93bb7b02cc
Gerivan Costa <119078732+gerivanc@users.noreply.github.com>
2025-11-24 10:05:48 -0400

    Update setup.py

--- src/xphrase/setup.py
+++ src/xphrase/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="xphrase",
-    version="1.0.2",
+    version="1.0.3",
     author="Gerivanc",
     author_email="your-email@example.com",
     description="Expressive phrase generator, strong, modern and minimalist",
