6a5c1fe93c7dd929818edbbc7c10e852078c0711
Gerivan Costa <119078732+gerivanc@users.noreply.github.com>
2025-11-25 09:23:40 -0400

    Update __init__.py

--- src/xphrase/data/__init__.py
+++ src/xphrase/data/__init__.py
@@ -3,7 +3,7 @@ Data module for XPhrase Generation
 Contains word lists for English, German, and Portuguese languages
 """
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from .english_words import ENGLISH_WORDS
 from .german_words import GERMAN_WORDS
